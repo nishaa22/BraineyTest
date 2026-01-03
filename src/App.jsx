35dd1b1c0ce828acb708637efad439a6ddc8786b
nisha <nishaa.1220@gmail.com>
2026-01-03 19:17:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <h1 class="text-3xl font-bold">
+        Hello Brainey
+      </h1>
+    </div>
+  )
+}
+
+export default App
